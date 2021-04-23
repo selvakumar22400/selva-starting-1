44ca28db288ca976bea7d27d008289523709f3a7
selvakumar22400 <78866668+selvakumar22400@users.noreply.github.com>
2021-04-23 21:57:43 +0530

    Add files via upload

---
+++ nested if else.py
@@ -0,0 +1,14 @@
+a=int(input("enter the value"))
+b=int(input("enter the value"))
+if a==5:
+    if a<b:
+        print("true")
+    else:
+        print('false')
+else:
+    print("terminate")
+
+    
+        
+    
+        
